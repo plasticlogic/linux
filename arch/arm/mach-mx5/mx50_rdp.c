1abdfe597ea319f39dc0b6862d2e4a173f2d986c
Anson Huang <b20788@freescale.com>
2011-05-16 11:37:33 +0800

    ENGR00143437 [MX50 RD3]DCDC_3V15 GPIO change
    Change GPIO setting of DCDC_3V15 for REV-D board.
    
    Signed-off-by: Anson Huang <b20788@freescale.com>

--- arch/arm/mach-mx5/mx50_rdp.c
+++ arch/arm/mach-mx5/mx50_rdp.c
@@ -2079,7 +2079,7 @@ static void mx50_suspend_enter()
 		/* Enable the Pull/keeper */
 		mxc_iomux_v3_setup_pad(iomux_setting);
 		gpio_request(DCDC_EN, "dcdc-en");
-		gpio_direction_output(DCDC_EN, 1);
+		gpio_direction_output(DCDC_EN, 0);
 	}
 
 	/* Set PADCTRL to 0 for all IOMUX. */
@@ -2113,7 +2113,7 @@ static void mx50_suspend_exit()
 		/* Enable the Pull/keeper */
 		mxc_iomux_v3_setup_pad(iomux_setting);
 		gpio_request(DCDC_EN, "dcdc-en");
-		gpio_direction_output(DCDC_EN, 0);
+		gpio_direction_output(DCDC_EN, 1);
 	}
 
 	mxc_iomux_v3_setup_multiple_pads(suspend_exit_pads,
