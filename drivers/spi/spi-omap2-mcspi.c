87d30eba57438123d87d68356abf7b51d981f328
Guillaume Tucker <guillaume.tucker@plasticlogic.com>
2013-05-15 16:02:56 +0100

    arm:omap:mcspi: add CONFIG_MODELF_PL_HARDWARE_MODULE
    The Plastic Logic hardware driver for the Epson-based displays
    can now be built ad a loadable module, so update the test to swap
    the SPI data lines for both built-in and loadable module
    configurations.
    
    Signed-off-by: Guillaume Tucker <guillaume.tucker@plasticlogic.com>

--- drivers/spi/spi-omap2-mcspi.c
+++ drivers/spi/spi-omap2-mcspi.c
@@ -680,7 +680,8 @@ static int omap2_mcspi_setup_transfer(struct spi_device *spi,
 	/* standard 4-wire master mode:  SCK, MOSI/out, MISO/in, nCS
 	 * REVISIT: this controller could support SPI_3WIRE mode.
 	 */
-#ifdef CONFIG_MODELF_PL_HARDWARE
+#if (defined(CONFIG_MODELF_PL_HARDWARE) \
+     || defined(CONFIG_MODELF_PL_HARDWARE_MODULE))
 	/* Z1.3 swaps MOSI and MISO from conventional use */
 	l &= ~(OMAP2_MCSPI_CHCONF_DPE0);
 	l |= OMAP2_MCSPI_CHCONF_IS|OMAP2_MCSPI_CHCONF_DPE1;
